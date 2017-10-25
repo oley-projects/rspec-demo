9019a685c09b3a6610acfe1f0c67558366b73ec9
Oley-Projects <oley.for.git@gmail.com>
2017-10-25 14:28:46 +0300

    add power_up test

--- hero_spec.rb
+++ hero_spec.rb
@@ -5,8 +5,12 @@ describe Hero do
 
   it "has a capitilized name" do
     hero = Hero.new 'mike'
-
     expect(hero.name).to eq 'Mike'
+  end
+
+  it "can power up" do
+    hero = Hero.new 'mike'
+    expect(hero.power_up).to eq 110
   end
 
 end
